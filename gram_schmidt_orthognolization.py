23c7eae5b558929ded0ac44d147cb45001c84d9c
IISCAditayTripathi <aditayt@iisc.ac.in>
2018-11-01 15:32:54 +0530

    Adding modified algorithm

--- gram_schmidt_orthognolization.py
+++ gram_schmidt_orthognolization.py
@@ -13,6 +13,10 @@ args = parser.parse_args()
 v1 = numpy.array([1, -1, 3])
 v2 = numpy.array([3, 2, 5])
 v3 = numpy.array([3, 1, 4])
+
+x = {'1':v1, '2':v2, '3': v3}
+u = {}
+
 if (args.mode == 'classical'):
     u1 = v1/numpy.linalg.norm(v1, ord=2)
     u2_dash = v2 - numpy.dot(v2,u1)*u1
@@ -23,3 +27,22 @@ if (args.mode == 'classical'):
     print('u1: [%f %f %f]' % (u1[0], u1[1], u1[2]))
     print('u2: [%f %f %f]' % (u2[0], u2[1], u2[2]))
     print('u3: [%f %f %f]' % (u3[0], u3[1], u3[2]))
+
+elif (args.mode == 'modified'):
+    for k in range(3):
+        if (k==0):
+            print (k)
+            u[str(k+1)] = x[str(k+1)]/numpy.linalg.norm(x[str(k+1)], ord=2)
+            print(u[str(k+1)])
+            for i in range(3-k-1):
+                u[str(i+1+k+1)] = x[str(i+1+k+1)]
+
+        else:
+            for i in range(3 - k - 1):
+                u[str(i + 1 + k + 1)] = u[str(i + 1 + k + 1)] - numpy.dot(numpy.transpose(u[str(i+1+k)]), u[str(i+1+k+1)])*u[str(i+1+k)]
+            u[str(k+1)] = u[str(k+1)]/numpy.linalg.norm(u[(str(k+1))], ord=2)
+            print(u[str(k+1)])
+
+
+
+
